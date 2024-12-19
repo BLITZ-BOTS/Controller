716b2db7a84291abe2522d4fbd3e7c40447dd00b
Char <char@charcodes.online>
2024-12-19 23:56:52 +0000

    Added better alerts

--- src/Components/Notification/index.tsx
+++ src/Components/Notification/index.tsx
@@ -1,35 +1,40 @@
 // Packages
 import { motion } from 'framer-motion';
 
+// Components
+import { Alert, AlertDescription, AlertTitle } from '@/Components/ui/alert';
+
 interface NotificationProps {
   message: string;
   type: 'loading' | 'success' | 'error';
 }
 
 const Notification = ({ message, type }: NotificationProps) => {
-  const backgroundColorClass =
+  const alertVariant =
     type === 'loading'
-      ? 'bg-yellow-500/10'
+      ? 'warning'
       : type === 'success'
-        ? 'bg-green-500/10'
-        : 'bg-red-500/10';
+        ? 'success'
+        : 'destructive';
 
-  const borderColorClass =
-    type === 'loading'
-      ? 'border-yellow-500'
-      : type === 'success'
-        ? 'border-green-500'
-        : 'border-red-500';
+  console.log(alertVariant);
 
   return (
     <motion.div
-      className={`${backgroundColorClass} border ${borderColorClass} backdrop-blur-md text-white fixed bottom-[50px] right-0 m-4 p-4 rounded-lg shadow-lg w-80`}
+      className="fixed bottom-[50px] right-0 m-4 w-80"
       initial={{ x: '100%' }}
       animate={{ x: 0 }}
       exit={{ x: '100%' }}
       transition={{ type: 'spring', stiffness: 300 }}
     >
-      <p>{message}</p>
+      <Alert variant={alertVariant || 'default'}>
+        <AlertTitle>
+          {type === 'loading' && 'Loading...'}
+          {type === 'success' && 'Success!'}
+          {type === 'error' && 'Error!'}
+        </AlertTitle>
+        <AlertDescription>{message}</AlertDescription>
+      </Alert>
     </motion.div>
   );
 };
